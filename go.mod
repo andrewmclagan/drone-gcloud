88a5064acab25a9a397dc215b5d0c839fee928d8
Andrew Mclagan <andrewmclagan@gmail.com>
2020-10-17 20:02:25 +1100

    golang ver

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/andrewmclagan/drone-gcloud
+
+require (
+	github.com/joho/godotenv v1.3.0
+	github.com/sirupsen/logrus v1.3.0
+	github.com/urfave/cli v1.22.2
+)
+
+go 1.15
---
+++ tag.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"os"
+	"os/exec"
+	"encoding/base64"
+	"encoding/json"
+
+	"github.com/sirupsen/logrus"
+	"github.com/urfave/cli"
+)
+
+var (
+	version = "unknown"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "gcloud plugin"
+	app.Usage = "gcloud plugin"
+	app.Action = run
+	app.Version = version
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:   "source",
+			Usage:  "source tag",
+			EnvVar: "PLUGIN_SOURCE",
+		},				
+		cli.StringFlag{
+			Name:   "dest",
+			Usage:  "destination tag",
+			EnvVar: "PLUGIN_DEST",
+		},		
+		cli.StringSliceFlag{
+			Name:     "repositories",
+			Usage:    "repositories to tag",
+			EnvVar:   "PLUGIN_REPOSITORIES",
+		},
+		cli.StringFlag{
+			Name:   "service_key",
+			Usage:  "GCP service key",
+			EnvVar: "PLUGIN_SERVICE_KEY",
+		},	
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		logrus.Fatal(err)
+	}
+}
+
+func decodeServiceKey(encodedKey string) []byte {
+	decodedKey, err := base64.StdEncoding.DecodeString(encodedKey)
+
+	if err != nil {
+		logrus.Error(err)
+	}			
+
+	logrus.Info("service key decoded")	
+
+    return decodedKey
+}
+
+func writeServiceKey(decodedKey []byte) {
+    f, err := os.Create("service_key.json")
+
+    if err != nil {
+        logrus.Fatal(err)
+    }
+
+    defer f.Close()
+
+    _, err2 := f.Write(decodedKey)
+
+    if err2 != nil {
+        logrus.Fatal(err2)
+    }
+
+	logrus.Info("service key written")	
+}
+
+func authenticate(decodedKey []byte) {
+	var serviceKey map[string]interface{}
+
+	json.Unmarshal([]byte(decodedKey), &serviceKey)
+
+	var clientEmail string = serviceKey["client_email"].(string)
+
+	var projectId string = serviceKey["project_id"].(string)
+
+	_, err := exec.Command("gcloud", "auth", "activate-service-account", clientEmail, "--key-file=service_key.json", "--project=" + projectId).Output()	
+
+	if err != nil {
+		logrus.Error(err)
+	}		
+	
+	logrus.Info("authenticated " + clientEmail)	
+}
+
+func run(c *cli.Context) {
+
+	decodedKey := decodeServiceKey(c.String("service_key"))
+
+	writeServiceKey(decodedKey)
+
+	authenticate(decodedKey)
+
+	for _, repo := range c.StringSlice("repositories") {
+		var src string = repo + ":" + c.String("source")
+
+		var dest string = repo + ":" + c.String("dest")
+
+		_, err := exec.Command("gcloud", "container", "images", "add-tag", src, dest).Output()
+
+		if err != nil {
+			logrus.Error(err)
+		}		
+
+		logrus.Info(src + " >> " + dest)	
+	}	
+}
